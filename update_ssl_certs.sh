799f0b2094d68bf04970aacee3831c407deefd47
Jason Hobbs <telnetdoogie@gmail.com>
2021-06-12 17:44:46 -0500

    Script for applying Let's Encrypt SSL certificates to UDMP from an SCP source

---
+++ update_ssl_certs.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+# Updates installed certs if new certs are present
+# Schedule as a daily cron job
+
+SRC_USER={username for SCP Host}
+SRC_HOST={SCP hostname}
+SRC_FOLDER='{SCP host folder for certs}'
+
+WORKING_PATH=/mnt/data/ssl_certs
+if [[ ! -d $WORKING_PATH ]]
+then
+    echo "\$WORKING_PATH '$WORKING_PATH' folder doesn't exist! exiting."
+    exit 1
+fi
+
+#filename on the source for the keyfile
+SRC_KEY=privkey.pem
+#filename on the source for the cert
+SRC_CERT=cert.pem
+
+DEST_PATH=/mnt/data/unifi-os/unifi-core/config
+DEST_KEY=unifi-core.key
+DEST_CERT=unifi-core.crt
+
+#Copy certs from SRC_HOST
+scp -i /root/.ssh/id_rsa $SRC_USER@$SRC_HOST:$SRC_FOLDER/$SRC_CERT $WORKING_PATH/$SRC_CERT > /dev/null 2>&1
+scp -i /root/.ssh/id_rsa $SRC_USER@$SRC_HOST:$SRC_FOLDER/$SRC_KEY $WORKING_PATH/$SRC_KEY > /dev/null 2>&1
+
+if [[ ! -f $WORKING_PATH/$SRC_CERT ]]
+then
+    echo "Certificate file '$SRC_CERT' not copied from host! exiting"
+    exit 1
+fi
+if [[ ! -f $WORKING_PATH/$SRC_KEY ]]
+then
+    echo "Key file '$SRC_KEY' not copied from host! exiting"
+    exit 1
+fi
+
+chmod 644 $WORKING_PATH/$SRC_KEY
+chmod 644 $WORKING_PATH/$SRC_CERT
+
+#Compare versions to see if an update is needed
+SRC_VER=`md5sum $WORKING_PATH/$SRC_CERT | awk '{ print $1 }'`
+DEST_VER=`md5sum $DEST_PATH/$DEST_CERT | awk '{ print $1 }'`
+
+if [ $SRC_VER == $DEST_VER ]; then
+    echo "LE Certificates match system; no action needed"
+  else
+    # Update is needed; move files and restart unifi
+    echo "LE Certificates are new; updating system certs"
+
+    # Backup previous files
+    echo "Backing Up $DEST_PATH/$DEST_KEY to $DEST_PATH/$DEST_KEY.bkup"
+    cp $DEST_PATH/$DEST_KEY $DEST_PATH/$DEST_KEY.bkup
+    echo "Backing Up $DEST_PATH/$DEST_CERT to $DEST_PATH/$DEST_CERT.bkup"
+    cp $DEST_PATH/$DEST_CERT $DEST_PATH/$DEST_CERT.bkup
+
+    # Update certs
+    echo "Overwriting $DEST_PATH/$DEST_KEY"
+    cp $WORKING_PATH/$SRC_KEY $DEST_PATH/$DEST_KEY
+    echo "Overwriting $DEST_PATH/$DEST_CERT"
+    cp $WORKING_PATH/$SRC_CERT $DEST_PATH/$DEST_CERT
+
+    # Restart unifi-os
+    unifi-os restart
+fi
+
+#cleaning up
+rm $WORKING_PATH/$SRC_KEY
+rm $WORKING_PATH/$SRC_CERT
+
+exit 0
