0b6293a571d29f6bf757a8e03db9e95d76cfa410
Jason Hobbs <telnetdoogie@gmail.com>
2021-06-16 02:55:04 -0500

    Update copy_certs.sh
    fix wrapping line

--- scripts/copy_certs.sh
+++ scripts/copy_certs.sh
@@ -16,17 +16,15 @@ if [ $CURRENT_VER == $PREVIOUS_VER ]; then
     echo "Certificates have not been updated, no action"
   else
     echo "Certificates have been updated; Copying to new location"
-        cp /usr/syno/etc/certificate/system/default/* /var/services/homes/admin/ssl_certs/
+    cp /usr/syno/etc/certificate/system/default/* /var/services/homes/admin/ssl_certs/
 
-        openssl pkcs12 -export -in $DESTINATION_PATH/cert.pem -inkey $DESTINATION_PATH/privkey.pem -out $DESTINATION_PATH/unifi.p12 -name unifi -CAfile $DE                     STINATION_PATH/fullchain.pem -caname root -password pass:aircontrolenterprise
+    openssl pkcs12 -export -in $DESTINATION_PATH/cert.pem -inkey $DESTINATION_PATH/privkey.pem -out $DESTINATION_PATH/unifi.p12 -name unifi -CAfile $DESTINATION_PATH/fullchain.pem -caname root -password pass:aircontrolenterprise
+    keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore $DESTINATION_PATH/keystore -srckeystore $DESTINATION_PATH/unifi.p12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias unifi -noprompt
 
-    keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore $DESTINATION_PATH/keystore -srckeystore $DE                     STINATION_PATH/unifi.p12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias unifi -noprompt
+    rm $DESTINATION_PATH/*.p12
 
-        rm $DESTINATION_PATH/*.p12
-
-        chown admin $DESTINATION_PATH/*
-        chmod 700 $DESTINATION_PATH/*
-
+    chown admin $DESTINATION_PATH/*
+    chmod 700 $DESTINATION_PATH/*
 fi
 
 exit 0
