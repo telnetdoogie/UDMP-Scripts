08834d4d21ab20716abb923b8911aa6245306ccc
Jason Hobbs <telnetdoogie@gmail.com>
2021-06-17 14:52:27 -0500

    destination path, alias, and passwords as variables

--- scripts/copy_certs.sh
+++ scripts/copy_certs.sh
@@ -9,6 +9,12 @@ SCP_USER={whichever user you'll pull the files as}
 FILE_TO_CHECK=cert.pem
 SYSTEM_CERT_PATH=/usr/syno/etc/certificate/system/default
 DESTINATION_PATH={place_to_drop_certs for scp from UDMP}
+# default keystorepass for Unifi, change if generating for some other system
+# KEYSTORE_PASS=aircontrolenterprise
+KEYSTORE_PASS=aircontrolenterprise
+# default alias for Unifi, change if generaing for some other system
+# KEYSTORE_ALIAS=unifi
+KEYSTORE_ALIAS=unifi
 
 CURRENT_VER=`md5sum $SYSTEM_CERT_PATH/$FILE_TO_CHECK | awk '{ print $1 }'`
 PREVIOUS_VER=`md5sum $DESTINATION_PATH/$FILE_TO_CHECK | awk '{ print $1 }'`
@@ -17,12 +23,12 @@ if [ $CURRENT_VER == $PREVIOUS_VER ]; then
     echo "Certificates have not been updated, no action"
   else
     echo "Certificates have been updated; Copying to new location"
-    cp /usr/syno/etc/certificate/system/default/* /var/services/homes/admin/ssl_certs/
+    cp $SYSTEM_CERT_PATH/* $DESTINATION_PATH/
 
-    openssl pkcs12 -export -in $DESTINATION_PATH/cert.pem -inkey $DESTINATION_PATH/privkey.pem -out $DESTINATION_PATH/unifi.p12 -name unifi -CAfile $DESTINATION_PATH/fullchain.pem -caname root -password pass:aircontrolenterprise
-    keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore $DESTINATION_PATH/keystore -srckeystore $DESTINATION_PATH/unifi.p12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias unifi -noprompt
+    openssl pkcs12 -export -in $DESTINATION_PATH/cert.pem -inkey $DESTINATION_PATH/privkey.pem -out $DESTINATION_PATH/temp.p12 -name $KEYSTORE_ALIAS -CAfile $DESTINATION_PATH/fullchain.pem -caname root -password pass:$KEYSTORE_PASS
+    keytool -importkeystore -deststorepass $KEYSTORE_PASS -destkeypass $KEYSTORE_PASS -destkeystore $DESTINATION_PATH/keystore -srckeystore $DESTINATION_PATH/temp.p12 -srcstoretype PKCS12 -srcstorepass $KEYSTORE_PASS -alias $KEYSTORE_ALIAS -noprompt
 
-    rm $DESTINATION_PATH/*.p12
+    rm $DESTINATION_PATH/temp.p12
 
     chown $SCP_USER $DESTINATION_PATH/*
     chmod 700 $DESTINATION_PATH/*
