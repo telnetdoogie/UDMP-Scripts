f48117721b5051b465b2f9565f69209e054088de
Jason Hobbs <telnetdoogie@gmail.com>
2021-08-22 00:53:08 -0500

    added path for keytool, since runs as cron user without keystore in path

--- scripts/copy_certs.sh
+++ scripts/copy_certs.sh
@@ -5,10 +5,12 @@
 # ...this is specifically written for a Synology NAS with generated LetsEncrypt certs
 # depends on the installation of Java 8 in Synology package manager
 
-SCP_USER={whichever user you'll pull the files as}
+SCP_USER=admin
 FILE_TO_CHECK=cert.pem
 SYSTEM_CERT_PATH=/usr/syno/etc/certificate/system/default
-DESTINATION_PATH={place_to_drop_certs for scp from UDMP}
+DESTINATION_PATH=/var/services/homes/admin/ssl_certs
+KEYTOOL_PATH=/var/packages/Java8/target/j2sdk-image/bin/
+
 # default keystorepass for Unifi, change if generating for some other system
 # KEYSTORE_PASS=aircontrolenterprise
 KEYSTORE_PASS=aircontrolenterprise
@@ -20,18 +22,23 @@ CURRENT_VER=`md5sum $SYSTEM_CERT_PATH/$FILE_TO_CHECK | awk '{ print $1 }'`
 PREVIOUS_VER=`md5sum $DESTINATION_PATH/$FILE_TO_CHECK | awk '{ print $1 }'`
 
 if [ $CURRENT_VER == $PREVIOUS_VER ]; then
-    echo "Certificates have not been updated, no action"
-  else
-    echo "Certificates have been updated; Copying to new location"
-    cp $SYSTEM_CERT_PATH/* $DESTINATION_PATH/
+  echo "Certificates have not been updated, no action"
+else
+  echo "Certificates have been updated; Copying to new location"
+  cp $SYSTEM_CERT_PATH/* $DESTINATION_PATH/
 
-    openssl pkcs12 -export -in $DESTINATION_PATH/cert.pem -inkey $DESTINATION_PATH/privkey.pem -out $DESTINATION_PATH/temp.p12 -name $KEYSTORE_ALIAS -CAfile $DESTINATION_PATH/fullchain.pem -caname root -password pass:$KEYSTORE_PASS
-    keytool -importkeystore -deststorepass $KEYSTORE_PASS -destkeypass $KEYSTORE_PASS -destkeystore $DESTINATION_PATH/keystore -srckeystore $DESTINATION_PATH/temp.p12 -srcstoretype PKCS12 -srcstorepass $KEYSTORE_PASS -alias $KEYSTORE_ALIAS -noprompt
+  openssl pkcs12 -export -in $DESTINATION_PATH/cert.pem -inkey $DESTINATION_PATH/privkey.pem \
+     -out $DESTINATION_PATH/temp.p12 -name $KEYSTORE_ALIAS -CAfile $DESTINATION_PATH/fullchain.pem \
+     -caname root -password pass:$KEYSTORE_PASS
 
-    rm $DESTINATION_PATH/temp.p12
+  $KEYTOOL_PATH/keytool -importkeystore -deststorepass $KEYSTORE_PASS -destkeypass $KEYSTORE_PASS \
+     -destkeystore $DESTINATION_PATH/keystore -srckeystore $DESTINATION_PATH/temp.p12 \
+     -srcstoretype PKCS12 -srcstorepass $KEYSTORE_PASS -alias $KEYSTORE_ALIAS -noprompt
 
-    chown $SCP_USER $DESTINATION_PATH/*
-    chmod 700 $DESTINATION_PATH/*
+  rm $DESTINATION_PATH/temp.p12
+  chown $SCP_USER $DESTINATION_PATH/*
+  chmod 700 $DESTINATION_PATH/*
+
 fi
 
 exit 0
